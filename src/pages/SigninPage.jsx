627de16193b613282ecc3847302bdf65ada5387d
RamyaUggumudi <144558944+RamyaUggumudi@users.noreply.github.com>
2024-10-28 14:34:56 +0530

    Add files via upload

---
+++ src/pages/SigninPage.jsx
@@ -0,0 +1,113 @@
+import { Box, Grid, colors } from "@mui/material";
+import React, { useState } from "react";
+import assets from "../assets";
+import SigninForm from "../components/SigninForm";
+import SignupForm from "../components/SignupForm";
+
+export const ScreenMode = {
+  SIGN_IN: "SIGN_IN",
+  SIGN_UP: "SIGN_UP",
+};
+
+const SigninPage = () => {
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState("unset");
+  const [width, setWidth] = useState(0);
+  const [backgroundImage, setBackgroundImage] = useState(
+    assets.images.signinBg
+  );
+  const [currMode, setCurrMode] = useState(ScreenMode.SIGN_IN);
+
+  const onSwitchMode = (mode) => {
+    setWidth(100);
+
+    const timeout1 = setTimeout(() => {
+      setCurrMode(mode);
+      setBackgroundImage(
+        mode === ScreenMode.SIGN_IN
+          ? assets.images.signinBg
+          : assets.images.signupBg
+      );
+    }, 1100);
+
+    const timeout2 = setTimeout(() => {
+      setLeft("unset");
+      setRight(0);
+      setWidth(0);
+    }, 1200);
+
+    const timeout3 = setTimeout(() => {
+      setRight("unset");
+      setLeft(0);
+    }, 2500);
+
+    return () => {
+      clearTimeout(timeout1);
+      clearTimeout(timeout2);
+      clearTimeout(timeout3);
+    };
+  };
+
+  return (
+    <Grid container sx={{ height: "100vh", width: "100vw" }}>
+      {/* Left panel */}
+      <Grid
+        item
+        xs={6}
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: 3,
+          position: "relative",
+        }}
+      >
+        {currMode === ScreenMode.SIGN_IN ? (
+          <SigninForm onSwitchMode={onSwitchMode} />
+        ) : (
+          <SignupForm onSwitchMode={onSwitchMode} />
+        )}
+        <Box
+          sx={{
+            position: "absolute",
+            top: 0,
+            left: left,
+            right: right,
+            width: `${width}%`,
+            height: "100%",
+            bgcolor: colors.grey[800],
+            transition: "all 1s ease-in-out",
+          }}
+        />
+      </Grid>
+
+      {/* Right panel */}
+      <Grid
+        item
+        xs={6}
+        sx={{
+          backgroundImage: `url(${backgroundImage})`,
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          position: "relative",
+        }}
+      >
+        <Box
+          sx={{
+            position: "absolute",
+            top: 0,
+            left: left,
+            right: right,
+            width: `${width}%`,
+            height: "100%",
+            bgcolor: colors.common.white,
+            transition: "all 1s ease-in-out",
+          }}
+        />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default SigninPage;
