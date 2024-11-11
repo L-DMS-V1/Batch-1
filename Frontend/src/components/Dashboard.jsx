8e213f7ac97b1296ce3b636a72e8bc4e683908ae
S BAVADHARINI <118330194+bavadharini09@users.noreply.github.com>
2024-11-11 20:53:58 +0530

    Update Dashboard.jsx

--- Frontend/src/components/Dashboard.jsx
+++ Frontend/src/components/Dashboard.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
-import './Dashboard.jsx'; 
+import React from "react";
+import "./Dashboard.css";
 
-const Dashboard = () => {
+function Dashboard() {
   return (
     <div className="dashboard">
-      <h2>Learning Hub Dashboard</h2>
-      <div className="card blue">1<br />Courses Created</div>
-      <div className="card green">1<br />Employees</div>
-      <div className="card purple">1<br />Requests</div>
+      <h1 className="dashboard-title">Dashboard</h1>
+
+      <div className="dashboard-box-container">
+        <div className="dashboard-box courses">1<br />Courses Created</div>
+        <div className="dashboard-box employees">1<br />Employees</div>
+        <div className="requests-container">
+          <button className="add-button">+ Add Employees</button>
+          <div className="dashboard-box requests">
+            1<br />Requests
+          </div>
+        </div>
+      </div>
     </div>
   );
-};
+}
 
 export default Dashboard;
